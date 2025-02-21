32fe180b4c94cbc9e49ab252fb3b9eabae05f379
Kai <ysmu_@hotmail.com>
2025-02-21 17:00:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.getElementById('randomWebsiteButton').addEventListener('click', () => {
-  fetch('websites.json')
-    .then(response => response.json())
-    .then(data => {
-      const randomWebsite = data[Math.floor(Math.random() * data.length)];
-      window.location.href = randomWebsite;
-    })
-    .catch(error => {
-      console.error('Error fetching website data:', error);
-    });
+    fetch('websites.json')
+        .then(response => response.json())
+        .then(data => {
+            const randomWebsite = data[Math.floor(Math.random() * data.length)];
+            window.open(randomWebsite, '_blank'); // Opens in a new tab
+        })
+        .catch(error => {
+            console.error('Error fetching website data:', error);
+        });
 });
